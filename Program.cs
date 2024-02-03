053ad82c0a2c580a48d640294cdcc27c3887cbe8
namfon <karnpitcha1408@hotmail.com>
2024-02-04 00:39:33 +0700

    Register account

--- Program.cs
+++ Program.cs
@@ -33,6 +33,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Product}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
